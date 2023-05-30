e76dcce797ffddc42199082507b2bbcaea41ac02
qquiche <ethanrbenson@gmail.com>
2023-05-30 01:32:21 -0500

    Added number of players form submission

--- pokerCalcApp/urls.py
+++ pokerCalcApp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('calcInput/', views.calcInput, name='calcInput'),
+    #path('calcOutput/', views.calcOutput, name='calcOutput'),
 ]
     
--- pokerCalcApp/views.py
+++ pokerCalcApp/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Card, numPlayers
+from pCalc import deck
+import pCalc
 # Create your views here.
 
 def index(request):
+    if request.method == 'POST':
+        numPlayers = request.POST.get('numPlayers') 
+        context = {'numPlayers': numPlayers, 'deck': deck}
+        return calcInput(request, context)
     return render(request, 'pokerCalcApp/index.html')
+def calcInput(request, context):
+    return render(request, 'pokerCalcApp/calcInput.html', context)
 
